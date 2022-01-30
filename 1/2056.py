7031dd44b3c5e701642e515acec753c1ad1e7db2
master <dlwoals0850@gmail.com>
2022-01-30 19:01:27 +0900

    2056fix

--- 1/2056.py
+++ 1/2056.py
@@ -1,19 +1,28 @@
-# T = int(input())
+T = int(input())
 
-# for i in range(T):
-#     target = list(input())
-#     if int(target[4]+target[5]) > 13:
-#         print(-1)
-#     elif int(target[5]+target[6]) in [1,3,5,7,8,10,12]:
-#         if int(target[7]+target[8]) > 31:
-#             print(-1)
-#     elif int(target[5]+target[6]) in [9,11,4,6] :
-#         if int(target[7]+target[8]) > 30:
-#             print(-1)
-#     elif int(target[5]+target[6]) == 2 :
-#         if int(target[7]+target[8]) > 28 :
-#             print(-1)
-#     else:
-#         print(f'#{i+1} {target[0]}{target{1}ta}')
+for i in range(T):
+ 
+    target = list(input())
 
-미완 보완필요
+    year = target[0]+target[1]+target[2]+target[3]
+    month = target[4]+target[5]
+    day = target[6]+target[7]
+    
+    if (int(target[4]+target[5]) > 13) or (int(target[4]+target[5]) <= 0) :
+        print(f'#{i+1} -1')
+    elif int(target[4]+target[5]) in [1,3,5,7,8,10,12]:
+        if int(target[6]+target[7]) > 31:
+            print(f'#{i+1} -1')
+        else : print(f'#{i+1} {year}/{month}/{day}')
+
+    elif int(target[4]+target[5]) in [9,11,4,6] :
+        if int(target[6]+target[7]) > 30:
+            print(f'#{i+1} -1')
+        else: print(f'#{i+1} {year}/{month}/{day}')
+
+    elif int(target[4]+target[5]) == 2 :
+        if int(target[6]+target[7]) > 28 :
+            print(f'#{i+1} -1')
+        else : print(f'#{i+1} {year}/{month}/{day}')
+    else :
+        print(f'#{i+1} -1')
